ebfd4fee40521f8a418cfab90cbe41e38177b073
Phruek Kosonsuwiwat <phruek.ko@gmail.com>
2024-04-27 18:44:19 +0700

    add predict_api

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-print("test")
-
 from flask import Flask,request, url_for, redirect, render_template, jsonify
 import pandas as pd
 import pickle
@@ -15,5 +13,15 @@ def home():
 def predict():
     return render_template('home.html',pred='You salary prediction will be {}'.format(100000))
 
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    # data = request.get_json(force=True)
+    # data_unseen = pd.DataFrame([data])
+    # prediction = predict_model(model, data=data_unseen)
+    # output = prediction.Label[0]
+    return jsonify(output)
+
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
