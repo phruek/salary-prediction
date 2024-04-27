feae745bb7c191b04dc1a2e9f069fcddaf467fe6
Phruek Kosonsuwiwat <phruek.ko@gmail.com>
2024-04-27 19:16:58 +0700

    add file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request, url_for, redirect, render_template, jsonify
+from flask import Flask,request, redirect, render_template, jsonify
 import pandas as pd
 import pickle
 import numpy as np
@@ -19,7 +19,7 @@ def predict_api():
     # data_unseen = pd.DataFrame([data])
     # prediction = predict_model(model, data=data_unseen)
     # output = prediction.Label[0]
-    return jsonify(output)
+    return jsonify()
 
 if __name__ == '__main__':
     app.run(debug=True)
